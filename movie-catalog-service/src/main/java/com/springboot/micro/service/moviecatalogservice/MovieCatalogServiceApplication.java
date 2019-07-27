d8f390279542c117c2993b5e631fb6638f59eecd
shi514652697 <ssnegi_4all@yahoo.com>
2019-07-27 11:25:11 -0500

    uupdated

---
+++ movie-catalog-service/src/main/java/com/springboot/micro/service/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.micro.service.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}
