d8e31f8ef8d3ad8e5810b9a4742f0c1a8d0228cb
shi514652697 <ssnegi_4all@yahoo.com>
2019-07-27 21:35:25 -0500

    updated

--- movie-catalog-service/src/main/java/com/springboot/micro/service/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/springboot/micro/service/moviecatalogservice/resources/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,11 +36,11 @@ public class MovieCatalogResource {
 		
 		//List<Rating> ratings = Arrays.asList(new Rating("1234", 4),new Rating("5678", 3));
 		
-		UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratingsdata/users/"+userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId, UserRating.class);
 		return ratings.getUserRating().stream().map(rating ->
 		{
 		
-			Movie movie= restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie= restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 			return new GetCatalogItem(movie.getName(), "Sultan desc", rating.getRating());
 			
 		}).collect(Collectors.toList());
